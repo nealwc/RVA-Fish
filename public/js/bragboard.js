94c71ce514d6a266c5a46a0e93c3d821490b06ce
GormTheWyrm <gdsoucie@gmail.com>
2020-05-23 13:38:31 -0400

    minor edits to brag.html, bragboard.html, created brag.js and breagboard.js

--- public/js/bragboard.js
+++ public/js/bragboard.js
@@ -1,1 +1,12 @@
-console.log("test");
+// console.log("test"); //worked!
+
+// ajax call gets the first x records
+
+
+$.get("/api/fish").then(function(data){
+console.log(data);
+
+
+});
+
+//create card with the info
